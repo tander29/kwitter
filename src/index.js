2d8f096e997882c51f7fcd778a8aa09a92a6604b
Travis Anderson <thetanderson@gmail.com>
2018-08-21 15:20:13 -0400

    practice

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+//need to do router work here provider/store,index to be rendered
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
