ce72afdfb33b71d091d79a4a49f9f30c2493f941
Travis Anderson <thetanderson@gmail.com>
2018-09-06 20:50:31 -0400

    update profile to have a date that is readable

--- src/Components/ProfileInfo.jsx
+++ src/Components/ProfileInfo.jsx
@@ -32,8 +32,10 @@ export class ProfileInfo extends Component {
     this.setState({ about: event.target.value })
   }
 
-
-
+  timeConversion = (messageTime) => {
+    let time = new Date(messageTime)
+    return time.toDateString()
+  }
 
   render() {
     return (
@@ -44,7 +46,7 @@ export class ProfileInfo extends Component {
         <Card.Content>
           <Card.Header>{this.props.displayName}</Card.Header>
           <Card.Meta>
-            <span className='date'>{this.props.createdAt}</span>
+            <span className='date'>Account Created: {this.timeConversion(this.props.createdAt)}</span>
           </Card.Meta>
           <Card.Description>{this.props.about}</Card.Description>
         </Card.Content>
